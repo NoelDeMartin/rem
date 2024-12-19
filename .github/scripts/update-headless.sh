e4d547b8e4d0397b1d01440ad4191587c22dcfe7
Noel De Martin <noel@noeldemartin.com>
2024-12-19 10:32:30 +0100

    Rename deployment folder to headless

--- .github/scripts/update-headless.sh
+++ .github/scripts/update-headless.sh
@@ -6,20 +6,20 @@ if [ -z $GITHUB_TOKEN ]; then
     exit 1
 fi
 
-git clone https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git headless -b headless
-rm headless/* -rf
-rm deployment/Dockerfile
-cp deployment/* headless/ -r
-cp deployment/.env.example headless/
-cp storage/ headless/ -r
+git clone https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git headless-gh -b headless
+rm headless-gh/* -rf
+rm headless/Dockerfile
+cp headless/* headless-gh/ -r
+cp headless/.env.example headless-gh/
+cp storage/ headless-gh/ -r
 
-if [[ -z `git -C headless status --short` ]]; then
+if [[ -z `git -C headless-gh status --short` ]]; then
     echo "No changes to apply!"
 
     exit;
 fi
 
-cd headless
+cd headless-gh
 git config user.name 'github-actions[bot]'
 git config user.email 'github-actions[bot]@users.noreply.github.com'
 git add -A
